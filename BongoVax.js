d8d7362739ca67d83b0a95af0d52c90fc39d8274
sazzad4677 <sazzad35-1898@diu.edu.bd>
2022-01-01 21:40:48 +0600

    final

--- BongoVax.js
+++ BongoVax.js
@@ -8,31 +8,28 @@ function vaxTrail(data) {
   data.map((value) => {
     if (value.age >= 20 && value.age < 30 && value.temperature < 100) {
       vaccineBooth.A.push(value);
-      vaccineBooth.A.sort((a, b) => a.age%2 - b.age%2 || a.age - b.age);
-    } 
-    else if (value.age >= 31 && value.age < 40 && value.temperature < 100) {
+      vaccineBooth.A.sort((a, b) => (a.age % 2) - (b.age % 2) || a.age - b.age);
+    } else if (value.age >= 31 && value.age < 40 && value.temperature < 100) {
       vaccineBooth.B.push(value);
-      vaccineBooth.B.sort((a, b) => a.age%2 - b.age%2 || a.age - b.age);
+      vaccineBooth.B.sort((a, b) => (a.age % 2) - (b.age % 2) || a.age - b.age);
     } else if (value.age >= 41 && value.age < 50 && value.temperature < 100) {
       vaccineBooth.C.push(value);
-      vaccineBooth.C.sort((a, b) => a.age%2 - b.age%2 || a.age - b.age);
+      vaccineBooth.C.sort((a, b) => (a.age % 2) - (b.age % 2) || a.age - b.age);
     } else if (value.temperature >= 100) {
       vaccineBooth.D.push(value);
-      vaccineBooth.D.sort((a, b) => a.age%2 - b.age%2 || a.age - b.age);
+      vaccineBooth.D.sort((a, b) => (a.age % 2) - (b.age % 2) || a.age - b.age);
     }
   });
   return vaccineBooth;
 }
 
-console.log(
-  vaxTrail([
-    { name: "SunilA", age: 21, temperature: 98 },
-    { name: "SunilA", age: 22, temperature: 98 },
-    { name: "BiplapB", age: 36, temperature: 98 },
-    { name: "BiplapB", age: 37, temperature: 98 },
-    { name: "BiplapC", age: 42, temperature: 98 },
-    { name: "BiplapC", age: 41, temperature: 98 },
-    { name: "BiplapD", age: 50, temperature: 100 },
-    { name: "BiplapD", age: 51, temperature: 100 },
-  ])
-);
+vaxTrail([
+  { name: "Biplap", age: 21, temperature: 98 },
+  { name: "Sunil", age: 22, temperature: 98 },
+  { name: "Kabir", age: 36, temperature: 99 },
+  { name: "Paul", age: 37, temperature: 99 },
+  { name: "Nayeem", age: 42, temperature: 98 },
+  { name: "Rahul", age: 41, temperature: 98 },
+  { name: "Kat", age: 50, temperature: 100 },
+  { name: "Sabnaj", age: 51, temperature: 101 },
+]);
